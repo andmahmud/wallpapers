bacc62660a1897ea75aded76de79ca9efca1e38d
MD.MAHMUDUL HASAN <126802456+andmahmud@users.noreply.github.com>
2025-12-29 21:55:59 +0600

    Update

---
+++ lib/custom_text.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final Color? color;
+  final TextAlign? textAlign;
+  final TextOverflow? overflow;
+  final int? maxLines;
+  final double? letterSpacing;
+  final double? height;
+  final TextDecoration? textDecoration;
+  final Color? textDecorationColor;
+
+  const CustomText({
+    super.key,
+    required this.text,
+    this.fontSize,
+    this.fontWeight,
+    this.color,
+    this.textAlign,
+    this.overflow,
+    this.maxLines,
+    this.letterSpacing,
+    this.height,
+    this.textDecoration,
+    this.textDecorationColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: textAlign,
+      overflow: overflow,
+      maxLines: maxLines,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        color: color,
+        letterSpacing: letterSpacing,
+        height: height,
+        decoration: textDecoration,
+        decorationColor: textDecorationColor,
+      ),
+    );
+  }
+}
